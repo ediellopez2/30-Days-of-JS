18bd44cfa95b5d3f8887c2051d08ea257c0a4676
EDIEL LOPEZ <elopez69@ucmerced.edu>
2022-06-12 16:11:01 -0700

    Day 7 Section 1 Exercises

--- 07_Day_Functions/07_day_starter/scripts/main.js
+++ 07_Day_Functions/07_day_starter/scripts/main.js
@@ -1,2 +1,45 @@
 console.log(countries)
 alert('Open the console and check if the countries has been loaded')
+
+function fullName1(name) {
+    console.log(`Hello, ${name}!`);
+}
+
+fullName1('Ediel Lopez');
+
+function fullName2(firstname, lastname) {
+    console.log(`Hi, ${firstname} ${lastname}`);
+}
+
+fullName2('Ediel', 'Lopez');
+
+function addNumbers(num1, num2) {
+    return num1 + num2;
+}
+
+console.log(addNumbers(13,17));
+
+function areaOfRectangle(len, wid) {
+    let area = len * wid;
+    return area;
+}
+
+console.log(areaOfRectangle(5,6));
+console.log(areaOfRectangle(7,7));
+
+
+function perimeterOfRectangle(len, wid) {
+    let per = 2 * (len + wid);
+    return per;
+}
+
+console.log(perimeterOfRectangle(5, 6));
+console.log(perimeterOfRectangle(7, 7));
+
+
+function volumeOfRectPrism(len, wid, height) {
+    let vol = len * wid * height;
+    return vol;
+}
+
+console.log(volumeOfRectPrism(3,4,5));
